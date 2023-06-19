3075f85892512c45269e3de0529b67985911eac7
AYTU ULIŞ <aytugulis@gmail.com>
2023-06-19 11:23:42 +0300

    initialize rio project

---
+++ classes/Inbox/index.ts
@@ -0,0 +1,23 @@
+import RDK, { Data, InitResponse, Response, StepResponse } from "@retter/rdk";
+
+const rdk = new RDK();
+
+export async function authorizer(data: Data): Promise<Response> {
+  return { statusCode: 200 };
+}
+
+export async function init(data: Data): Promise<InitResponse> {
+  return { state: { public: { messages: [] } } };
+}
+
+export async function getState(data: Data): Promise<Response> {
+  return { statusCode: 200, body: data.state };
+}
+
+export async function sayHello(data: Data): Promise<StepResponse> {
+  data.response = {
+    statusCode: 200,
+    body: { message: data.state.public.messages },
+  };
+  return data;
+}
